c2952e895ed477c7a05d6358ed6ef6dd6d34d281
Igor Banduric <banduric@hotmail.com>
2018-02-24 20:50:06 +0100

    Preformatovanie

--- src/ZajacieUsi.java
+++ src/ZajacieUsi.java
@@ -2,11 +2,11 @@ public class ZajacieUsi {
 
 	/**
 	 * Metoda vrati pocet uci zajacov
+	 *
 	 * @param pocetZajacov
 	 * @return pocet usi
 	 */
 	public int zajacieUsi(int pocetZajacov) {
-
 	}
 
 }
