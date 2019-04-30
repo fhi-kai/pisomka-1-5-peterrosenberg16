b185a85e5ca55af1a2f9f3e58f560e27e66435e3
Peter Rosenberg <36486676+peterrosenberg16@users.noreply.github.com>
2019-04-30 17:00:45 +0200

    Update Zajace.java

--- src/Zajace.java
+++ src/Zajace.java
@@ -2,10 +2,6 @@ public class Zajace implements ZajacieUsi{
 
     @Override
     public int zajacieUsi(int pocetZajacov) {
-        zajacieUsi(0);
-        zajacieUsi( 1);
-        zajacieUsi(2);
-        zajacieUsi(3);
-        return pocetZajacov;
-
+        if (pocetZajacov == 0) return 0;
+        return 2 + zajacieUsi(pocetZajacov-1);
     }}
